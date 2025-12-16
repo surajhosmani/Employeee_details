5cab6e7c3aab2cb51a339314ab6e34192fe1206e
surajhosmani <surajhosmani83@gmail.com>
2025-12-16 22:27:32 +0530

    Update test_emp.py

--- test_emp.py
+++ test_emp.py
@@ -2,10 +2,10 @@ from emp import emp_details
 
 def test_emp_details():
     expected_op = (
-        "Employee name: Queen\n"
+        "Employee name: Suraj\n"
         "Employee id: 900\n"
         "Department: MCA\n"
         "Salary: 200000"
     )
 
-    assert emp_details("Queen", 900, "MCA", 200000) == expected_op
+    assert emp_details("Suraj", 900, "MCA", 200000) == expected_op
